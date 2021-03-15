44131bc50aeda31bb46a33dd141e08d95a986142
JC <jc>
2021-03-15 16:50:12 +0000

    adding files

--- create_ansible_user.sh
+++ create_ansible_user.sh
@@ -4,13 +4,13 @@ USERNAME="ansible"
 PASSWORD="ansible"
 
 # adding ansible
-useradd $USERNAME
+sudo useradd $USERNAME
 
 # setting password for user: ansible
-echo -e "$PASSWORD\n$PASSWORD" | passwd $USERNAME 2>/dev/null
+sudo echo -e "$PASSWORD\n$PASSWORD" | passwd $USERNAME 2>/dev/null
 
 # setting bash shell as a default shell
-usermod --shell /bin/bash $USERNAME
+sudo usermod --shell /bin/bash $USERNAME
 
 # giving ansible user access to sudo
-echo "ansible ALL=(ALL)    NOPASSWD:ALL" >> /etc/sudoers
+sudo echo "ansible ALL=(ALL)    NOPASSWD:ALL" >> /etc/sudoers
